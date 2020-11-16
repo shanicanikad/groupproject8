497eb390d0256afeaa3f5aa4f376f982f52016b7
SHANICA_DENIS <shanicanikad@gmail.com>
2020-11-15 23:27:29 -0500

    updated Schema

--- models/WomenFashion.js
+++ models/WomenFashion.js
@@ -1,7 +1,7 @@
 const mongoose = require("../db/connections")
 const Schema = mongoose.Schema
 
-const WomenFashion = new Schema
+const FashionBrand = new Schema
 
 
     ({
@@ -10,77 +10,37 @@ const WomenFashion = new Schema
         CEO: String,
         type: String,
         price_point: String,
-        categories: [{
-            tops: [{
-                product_name1: String,
-                image_url1: String,
-                detail1: String,
-                product_name2: String,
-                image_url2: String,
-                detail2: String,
-                product_name3: String,
-                image_url3: String,
-                detail3: String,
-                product_name4: String,
-                image_url4: String,
-                detail4: String,
-                product_name5: String,
-                image_url5: String,
-                details5: String
+        categories: [
+            {
+                category: String,
+                products: [
+                    {
+                        name: String,
+                        image_url: String,
+                        details: String,
+                    },
+                    {
+                        name: String,
+                        image_url: String,
+                        details: String,
+                    },
+                    {
+                        name: String,
+                        image_url: String,
+                        details: String,
+                    },
+                    {
+                        name: String,
+                        image_url: String,
+                        details: String,
+                    },
+                    {
+                        name: String,
+                        image_url: String,
+                        details: String
+                    }],
             }],
-            bottoms: [{
-                product_name1: String,
-                image_url1: String,
-                detail1: String,
-                product_name2: String,
-                image_url2: String,
-                detail2: String,
-                product_name3: String,
-                image_url3: String,
-                detail3: String,
-                product_name4: String,
-                image_url4: String,
-                detail4: String,
-                product_name5: String,
-                image_url5: String,
-                details5: String
-            }],
-            shoes: [{
-                product_name1: String,
-                image_url1: String,
-                detail1: String,
-                product_name2: String,
-                image_url2: String,
-                detail2: String,
-                product_name3: String,
-                image_url3: String,
-                detail3: String,
-                product_name4: String,
-                image_url4: String,
-                detail4: String,
-                product_name5: String,
-                image_url5: String,
-                details5: String
-            }],
-            accessories: [{
-                product_name1: String,
-                image_url1: String,
-                detail1: String,
-                product_name2: String,
-                image_url2: String,
-                detail2: String,
-                product_name3: String,
-                image_url3: String,
-                detail3: String,
-                product_name4: String,
-                image_url4: String,
-                detail4: String,
-                product_name5: String,
-                image_url5: String,
-                details5: String
-            }]
-        }],
         link: String
     })
 
-    module.exports = mongoose.model("WomenFashion", WomenFashion)
+module.exports = mongoose.model("FashionBrand", FashionBrand)
